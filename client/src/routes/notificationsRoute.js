ea5da6100b3cd5c416257dffcd389244005cf966
danieldayton33 <43708095+danieldayton33@users.noreply.github.com>
2019-05-07 20:32:18 -0400

    Revert "List item"

---
+++ client/src/routes/notificationsRoute.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import Notifications from '../components/notifications/Notifications';
+
+const notificationsRoute = () => {
+ return (
+  <Fragment>
+   <Notifications />
+  </Fragment>
+ )
+}
+
+export default notificationsRoute
