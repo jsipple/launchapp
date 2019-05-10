80e66d09d2cd77eee5d7fd662a2f1e8401eba9b3
unknown <sipple3@gmail.com>
2019-05-10 11:49:51 -0400

    made changes to server file for heroku

--- server/server.js
+++ server/server.js
@@ -48,6 +48,15 @@ app.use('/api', apiRoutes)
 // https.createServer(, (req, res) => {
 
 // })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(express.static('public'))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/public/index.html"));
+});
 
 app.listen(PORT, () => {
     console.log('app listening on port ' + PORT)
