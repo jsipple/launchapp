1967567e172cfd2c3a5e2436c2e4cb5813981655
unknown <sipple3@gmail.com>
2019-05-22 17:45:34 -0400

    removed requirement of redux dev tools

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -18,14 +18,10 @@ const initialState = {
     image: ''
 };
 
-const middleware = [thunk];
-const allEnhancers = compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
 
 const store = createStore(
     launchReducer,
     initialState,
-    allEnhancers
 );
 
 export default store;
